8514ce86a977aa73f1dc7b4dcbb5ec644ddbfb6a
usiyoung <usiyoung7@gmail.com>
2022-01-17 00:20:43 +0900

    about mock

---
+++ mock/check/test/check.test.js
@@ -0,0 +1,29 @@
+const check = require('../check.js');
+
+describe('check',()=>{
+    let onSuccess;
+    let onFail;
+
+    beforeEach(()=>{
+        onSuccess = jest.fn();
+        onFail = jest.fn();
+    })
+
+    it('should call onSuccess when predicate is ture',()=>{
+        check(()=> true,onSuccess,onFail);
+        // expect(onSuccess.mock.calls.length).toBe(1);
+        expect(onSuccess).toHaveBeenCalledTimes(1);
+        expect(onSuccess).toHaveBeenCalledWith('yes');
+        // expect(onFail.mock.calls.length).toBe(0);
+        expect(onFail).toHaveBeenCalledTimes(0);
+
+    })
+    it('should call onFail when predicate is false',()=>{
+        check(()=>false,onSuccess,onFail);
+        // expect(onSuccess.mock.calls.length).toBe(0);
+        expect(onSuccess).toHaveBeenCalledTimes(0);
+        // expect(onFail.mock.calls.length).toBe(1);
+        expect(onFail).toHaveBeenCalledWith('no');
+        expect(onFail).toHaveBeenCalledTimes(1);
+    })
+})
